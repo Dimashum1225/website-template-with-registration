717fecd6458193bfd3d480608d81b159a5384bf6
Dimashum1225 <95349865+Dimashum1225@users.noreply.github.com>
2023-09-15 08:51:17 +0600

    Add files via upload

---
+++ routes/dashboardRoutes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const authMiddleware = require('../middlewares/authMiddleware');
+
+router.get('/',authMiddleware.requireLogin,(req,res)=>{
+    res.render('index',{session:req.session});
+});
+module.exports=router;
